c3da3b48c8291bfd3cf3e3905a3adcecaff0eef8
Noonlord <mymmym22@gmail.com>
2021-12-31 13:50:40 +0300

    courbet: AOSP-ify

--- aosp_courbet.mk
+++ aosp_courbet.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_courbet
+PRODUCT_NAME := aosp_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
